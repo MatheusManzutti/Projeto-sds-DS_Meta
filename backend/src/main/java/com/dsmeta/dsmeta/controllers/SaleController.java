b7f9269a30d33910257833117afdd7b869935939
Matheus Manzutti <mateusmanzutti@gmail.com>
2022-07-13 20:48:44 -0300

    Date select

--- backend/src/main/java/com/dsmeta/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/dsmeta/dsmeta/controllers/SaleController.java
@@ -1,10 +1,11 @@
 package com.dsmeta.dsmeta.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dsmeta.dsmeta.entities.Sale;
@@ -18,8 +19,12 @@ public class SaleController {
 	private SaleService service;
 	
 	@GetMapping //ANNOTATION PARA RESPONDER VIA WEB, USANDO HTTP.
-	public List<Sale> findSales() {
-		return service.findSales();
+	public Page<Sale> findSales(
+			//ANNOTATION PARA INFORMAR SE NÃO FOR INFORMADO O NOME DA REQUISIÇÃO (minDate; maxDate), VAI VOLTA VAZIO "" 
+								@RequestParam(value = "minDate", defaultValue = "") String minDate, 
+								@RequestParam(value = "maxDate", defaultValue = "") String maxDate, 
+								Pageable pageable) { //PARÂMETRO QUE VOLTA UMA BUSCA PAGINADA
+		return service.findSales(minDate, maxDate, pageable); 
 	}
 
 }
--- backend/src/main/java/com/dsmeta/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/dsmeta/dsmeta/services/SaleService.java
@@ -1,8 +1,12 @@
 package com.dsmeta.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.dsmeta.dsmeta.entities.Sale;
@@ -14,7 +18,42 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
-	public List<Sale> findSales() {
-		return repository.findAll(); //BUSCA UMA LISTA DE TODOS OS DADOS NO BD.
+	 //BUSCA UMA LISTA DE TODOS OS DADOS NO BD.
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+
+		//LocalDate = CONVERTE AS DATAS QUE ESTÃO EM STRING.
+		//LocalDate.parse = CONVERTE A STRING PARA A DATA NO LocalDate.
+		
+		//PEGA A DATA ATUAL
+		LocalDate today = LocalDate.ofInstant(Instant.now(), //PEGA O INSTANTE ATUAL
+											  ZoneId.systemDefault() //PEGA O FUSO HORÁRIO DO SISTEMA
+											 );
+				
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		/*minDate.equals("") = TESTA SE O MINDATE É IGUAL A UM TEXTO VAZIO (NÃO INFORMADO).
+		  ? today.minusDays(365) = SE FOR VERDADE VOLTA A DATA DE UM ANO ATRÁS, SENÃO VOLTA A DATA DO LOCALDATE.
+		  OU SEJA, O minusDays SUBTRAI OS DIAS*/
+		
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		/*maxDate.equals("") = TESTA SE O MAXDATE É IGUAL A UM TEXTO VAZIO (NÃO INFORMADO).
+		  ? today = SE FOR VERDADE VOLTA A DATA ATUAL, SENÃO VOLTA A DATA DO LOCALDATE.*/
+
+		
+		return repository.findSales(min, max, pageable);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
